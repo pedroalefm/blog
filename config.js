5866c226c3bb90a8d21fa344c2a44c5242dfaddb
Pedro Alef <pedro.alef>
2020-08-07 20:06:11 -0300

    change blog template

---
+++ config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+	url: 'http://pedroalefm.com',
+	pathPrefix: '/',
+	title: 'Blog by Pedro Alef Martins',
+	subtitle: 'FullStack Developer',
+	copyright: '© All rights reserved.',
+	disqusShortname: '',
+	postsPerPage: 4,
+	googleAnalyticsId: 'UA-73379983-2',
+	useKatex: false,
+	menu: [
+		{
+			label: 'Artigos',
+			path: '/',
+		},
+	],
+	author: {
+		name: 'Pedro Alef Martins',
+		photo: '/photo.jpg',
+		bio:
+			'Sou desenvolvedor FullStack Developer trabalhando com a stack React, React Native e Node.js. Além de tudo que cerca esse ecossistema.',
+		contacts: {
+			email: '',
+			facebook: 'pedroalefm',
+			telegram: '#',
+			twitter: 'pedroalefm',
+			github: 'pedroalefm',
+			rss: '',
+			vkontakte: '',
+			linkedin: '#pedroalefm',
+			instagram: 'pedroalefm',
+			line: '',
+			gitlab: '',
+			weibo: '',
+			codepen: '',
+			youtube: '',
+			soundcloud: '',
+		},
+	},
+};
